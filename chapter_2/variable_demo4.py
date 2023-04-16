4621e71f604bae23c9d409d24162a463638e6d74
Fourteen98 <emuhiydeen6@gmail.com>
2023-04-16 08:41:30 +0000

    add naming and variables

---
+++ chapter_2/variable_demo4.py
@@ -0,0 +1,11 @@
+dollars = 2.75
+print('Pastor Ernest says i have', dollars, 'in my account')
+
+dollars = 99.95
+print('Pastor Ernest now has', dollars)
+
+first_name = "Ernest"
+last_name = "Ofosu"
+amount = "$80,0000"
+
+print("Pastor", first_name, last_name, "has", amount)
