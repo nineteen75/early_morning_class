08e569fe885617bf99e7b7cb3f650ecf24cbcad1
Fourteen98 <emuhiydeen6@gmail.com>
2023-05-16 19:35:17 +0000

    indentation

---
+++ week_4/chapter_3/indentation.py
@@ -0,0 +1,42 @@
+# we have indentation in 2 level
+'''
+world
+  Africa
+    Ghana
+      Accra
+        cirle
+        Lapaz
+      Kumasi
+      Koforidua
+    Togo
+    South Africa
+  Asia
+    China
+    Malaysia
+    Kurdistan
+  Australia
+    Australia
+  Europe
+    England
+    Sweden
+
+'''
+
+# this is two level
+'''
+Ghana
+    koforidua
+        Asok)re
+        poly
+    kumasi
+
+'''
+# most use 2 level for indentation
+# some people also use 4 level
+
+
+def greeting():
+    print('Good Morning')
+
+def hello():
+    print('hello')
