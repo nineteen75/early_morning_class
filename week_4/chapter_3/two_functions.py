12b72fdcf0a890ec5654c934f8a4f2c2574cac24
Fourteen98 <emuhiydeen6@gmail.com>
2023-05-16 19:20:45 +0000

    more function

--- week_4/chapter_3/two_functions.py
+++ week_4/chapter_3/two_functions.py
@@ -1,12 +1,24 @@
 def divide():
     return 6 / 3
 
+
 def multiply():
     return 6 * 3
+
 
 def add():
     return 6 + 3
 
+
+def sayHello():
+    print('Hello guys')
+
+
 def main():
-    print("Dividing 6 by 3 %d", divide())
-    print("Multiplying 6 by 3")
+    sayHello()
+    print("Dividing 6 by 3 is ", divide())
+    print("Multiplying 6 by 3 is ", multiply())
+    print("Addding 6 to 3 is ", add())
+
+
+main()
