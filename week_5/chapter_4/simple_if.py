c85a0f04474dc79ea7e0ca9f2921cbd0530cb832
Fourteen98 <emuhiydeen6@gmail.com>
2023-05-21 06:17:38 +0000

    add simple if

---
+++ week_5/chapter_4/simple_if.py
@@ -0,0 +1,11 @@
+def authenticate_person(person):
+    if person == "Pastor Ernest":
+        return "welcome Pastor"
+    else:
+        return "Who are you?"
+
+def main():
+    person = "Pastor Ernest"
+    print(authenticate_person(person));
+
+main()
