a2dd8b51fb31c6e9c54c8c9cbdfe78de1f22d454
Fourteen98 <emuhiydeen6@gmail.com>
2023-05-28 07:08:39 +0000

    add ranges

---
+++ week_6/chapter_5/range.py
@@ -0,0 +1,15 @@
+def main():
+    print('I will display the numbers 1 through 5')
+
+    # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # for number in range(10):
+    #   print(number)
+
+    # for number in range(4, 10):
+    #     print(number)
+    
+    for number in range(2, 10, 2):
+        print(number)
+
+
+main()
