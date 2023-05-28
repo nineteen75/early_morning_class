c0b40232e479ea523f1ba68ec2d2e066b9f872c4
Fourteen98 <emuhiydeen6@gmail.com>
2023-05-28 07:00:35 +0000

    add odd.py

---
+++ week_6/chapter_5/odd.py
@@ -0,0 +1,10 @@
+def main():
+    print('I will display the numbers 1 through 5')
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for number in numbers:
+        if number % 2 != 0:
+            print(number)
+
+
+main()
