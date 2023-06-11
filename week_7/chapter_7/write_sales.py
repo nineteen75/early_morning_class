3b07ba155aba70d355687a2a2bd951882ced90c4
Fourteen98 <emuhiydeen6@gmail.com>
2023-06-11 08:30:02 +0000

    add writes_sales.py

---
+++ week_7/chapter_7/write_sales.py
@@ -0,0 +1,15 @@
+def main():
+    number_days = int(input('For how many days to you have sales: '))
+
+    sales_file = open('sales.txt', 'w')
+
+    for count in range(1, number_days + 1):
+        sales = input('Enter the sales for day %i: ' %count)
+
+        sales_file.write(str(sales) + '\n')
+
+    sales_file.close()
+    print('Data written to sales.txt')
+
+main()
+
